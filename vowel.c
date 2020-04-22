0e61d3a2246ba93f683a150a2bbbf4ff6bcd5fdf
Krishna Pratap Singh Rathore <31449127+kps123ips@users.noreply.github.com>
2020-04-22 16:23:16 +0530

    Add files via upload

---
+++ vowel.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	char ch;
+	printf("Enter Character");
+	scanf("%c",&ch);
+	if((ch=='A')||(ch=='a')||(ch=='E')||(ch=='e')||(ch=='I')||(ch=='i')||(ch=='O')||(ch=='o')||(ch=='U')||(ch=='u'))
+	printf("Character is Vowel %c ");
+	else
+	printf("Character is Consonant %c ");
+	getch();
+}
